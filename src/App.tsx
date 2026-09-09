17d3a6f64faaa822a80256eeef6bc5bc5520b63d
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-09-09 11:28:50 +0600

    Create a new project for oction players

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+      <button className="btn btn-error">Error</button>
+      <p className="text-2xl text-red-500">bpl</p>
+    </>
+  )
+}
+
+export default App
