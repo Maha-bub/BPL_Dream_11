607bf3c11c412f1e3491e06593421212059e4931
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-09-10 09:46:40 +0600

    Remove available players

--- src/components/Players/Players.tsx
+++ src/components/Players/Players.tsx
@@ -1,19 +1,19 @@
 import { use } from "react";
 import type { IplayerType } from "../PlayersType";
-import AvailablePlayers from "./AvailablePlayers";
 
-const Players = ({ PlayerDataPromise }: Promise<IplayerType[]>) => {
+
+interface PlayerProps {
+    PlayerDataPromise: Promise<IplayerType[]>
+}
+
+const Players = ({ PlayerDataPromise }: PlayerProps) => {
     const playerData = use(PlayerDataPromise);
     console.log(playerData)
 
     return (
         <div>
             Players:{playerData.length}
-            {
-                PlayerDataPromise.map((player: IplayerType, idx: number) => {
-                    return <AvailablePlayers key={idx} player={player}></AvailablePlayers>
-                })
-            }
+
         </div>
     );
 };
